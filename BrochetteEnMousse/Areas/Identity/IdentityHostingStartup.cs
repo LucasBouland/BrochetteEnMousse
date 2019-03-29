336c50697f1e8b507f1ef1d10b66e78b2b8607d0
Moustanir <moustanir.soule-ali@ynov.com>
2019-03-30 00:12:44 +0100

    Add pseudo field in view

---
+++ BrochetteEnMousse/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MousseModels.Data;
+using MousseModels.Models;
+
+[assembly: HostingStartup(typeof(BrochetteEnMousse.Areas.Identity.IdentityHostingStartup))]
+namespace BrochetteEnMousse.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
