caf9b3a563eb7357d18d46319e26c9e0964ea8ea
unknown <guillaume.arsac@hotmail.fr>
2019-04-18 15:40:23 +0200

    ne marche pas

---
+++ Api/Controllers/SessionsController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MousseModels.Data;
+using MousseModels.Models;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SessionsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SessionsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Sessions
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Session>>> GetSessions()
+        {
+            return await _context.Sessions.ToListAsync();
+        }
+
+        // GET: api/Sessions/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Session>> GetSession(string id)
+        {
+            var session = await _context.Sessions.FindAsync(id);
+
+            if (session == null)
+            {
+                return NotFound();
+            }
+
+            return session;
+        }
+
+        // PUT: api/Sessions/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutSession(string id, Session session)
+        {
+            if (id != session.ID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(session).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SessionExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Sessions
+        [HttpPost]
+        public async Task<ActionResult<Session>> PostSession(Session session)
+        {
+            _context.Sessions.Add(session);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetSession", new { id = session.ID }, session);
+        }
+
+        // DELETE: api/Sessions/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Session>> DeleteSession(string id)
+        {
+            var session = await _context.Sessions.FindAsync(id);
+            if (session == null)
+            {
+                return NotFound();
+            }
+
+            _context.Sessions.Remove(session);
+            await _context.SaveChangesAsync();
+
+            return session;
+        }
+
+        private bool SessionExists(string id)
+        {
+            return _context.Sessions.Any(e => e.ID == id);
+        }
+    }
+}
