31fef23a9ddad1551ba920408f5a16f1ea5e90ed
EliriosAebh <julien.lapa@ynov.com>
2019-04-25 02:18:32 +0200

    Create tag helper and debug todo

--- BrochetteEnMousse/wwwroot/js/site.js
+++ BrochetteEnMousse/wwwroot/js/site.js
@@ -2,3 +2,14 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$('.open-link a').click(function () {
+    if ($(".desc-wrap").css("height") === "60px") {
+        $('.desc-wrap').css('height', 'auto');
+        var autoHeight = $('.desc-wrap').height();
+        $('.desc-wrap').css('height', '60px');
+        $(".desc-wrap").animate({ height: autoHeight }, 1000);
+    } else {
+        $(".desc-wrap").animate({ height: "60px" }, 1000);
+    }
+    return false;
+});
